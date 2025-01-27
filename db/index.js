ecfe323b2cc409b97159d6f12e5be217b499838a
maynul69 <maynulhossain69@gmail.com>
2025-01-27 15:57:50 +0600

    add db

--- db/index.js
+++ db/index.js
@@ -2,12 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.PG_HOST || 'localhost',
-  port: process.env.PG_PORT || 5432,
-  user: process.env.PG_USER || 'postgres',
-  password: process.env.PG_PASSWORD || 'password',
-  database: process.env.PG_DATABASE || 'mydatabase',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // Enable SSL for production
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
